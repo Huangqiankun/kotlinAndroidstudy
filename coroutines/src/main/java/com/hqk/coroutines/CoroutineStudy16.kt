6cf603f0a15475b8ab0b5d5743e288e568756589
Brave <1359602169@qq.com>
2021-11-21 17:20:49 +0800

    11.21 update

--- coroutines/src/main/java/com/hqk/coroutines/CoroutineStudy16.kt
+++ coroutines/src/main/java/com/hqk/coroutines/CoroutineStudy16.kt
@@ -12,16 +12,16 @@ fun main() = runBlocking<Unit> {
         // 当使⽤ GlobalScope 来启动⼀个协程时，则新协程的作业没有⽗作业。
         //因此它与这个启动的作⽤域⽆关 且独⽴运作。
         GlobalScope.launch {
-            println("job1: I run in GlobalScope and execute independently!")
+            log("job1: I run in GlobalScope and execute independently!")
             delay(2000)
-            println("job1: I am not affected by cancellation of the request")
+            log("job1: I am not affected by cancellation of the request")
         }
 
         launch {
             delay(100)
-            println("job2: I am a child of the request coroutine")
+            log("job2: I am a child of the request coroutine")
             delay(2000)
-            println("job2: I will not execute this line if my parent request is cancelled")
+            log("job2: I will not execute this line if my parent request is cancelled")
         }
 
     }
@@ -29,7 +29,7 @@ fun main() = runBlocking<Unit> {
     delay(500)
     request.cancel()
     delay(5000)
-    println("main: Who has survived request cancellation?")
+    log("main: Who has survived request cancellation?")
 }
 
 
